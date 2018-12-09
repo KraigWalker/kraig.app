13879193e5a0e02699f4fbbea3270ef4eda7757f
Kraig Walker <kraig_walker@me.com>
2018-12-09 12:02:59 +0000

    Publishing to gh-pages

---
+++ precache-manifest.af2b7615dbfdfcb4bc27383373e0d6b0.js
@@ -0,0 +1,6 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd98e6a00e1fce8363ae",
+    "url": "/main.a6178c57b4344a1dea19.js"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,3 @@
+importScripts("/precache-manifest.af2b7615dbfdfcb4bc27383373e0d6b0.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+
