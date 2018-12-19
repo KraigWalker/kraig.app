a316be95eaaf1317ccbff0093c7caf3248582e6a
Kraig Walker <kraig_walker@me.com>
2018-12-19 15:29:59 +0000

    cache critical css

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,46 @@ if ('serviceWorker' in navigator) {
         })
     })
 }
+
+/**
+ * Enable a button inviting the user to preload the article.
+ * check caches for 
+ */
+function addPreloadHint() {
+    const btns = document.querySelectorAll('button[data-pre-status]');
+    
+    btns.forEach((element, key, parent) => {
+        switch(element.dataset.preload) {
+            case 1: {
+                // preload available
+                element.classList.add('pr-on');
+                // add click handler
+                element.addEventListener('click', handleUserPreloadRequest.bind(element));
+            }
+            case 2: {
+                // preloaded by user
+                element.classList.add('pr-done');
+            }
+            case 0:
+            default: {
+                // preload disabled
+                element.classList.add('pr-off');
+            }
+        }
+    });
+}
+
+/**
+ * When the user taps the preload button, instruct the ServiceWorker to precache relevant resources
+ */
+function handleUserPreloadRequest() {
+
+    navigator.serviceWorker.controller.postMessage()
+
+    // fetch the data-preload-url,
+    // if response is json:
+        // parse and fetch all subresources
+    // else
+    // add response (text/html) to cache
+    // When done, signal completion to the page
+}
--- src/js/sw.js
+++ src/js/sw.js
@@ -1,1 +1,44 @@
-console.log('hello world');
+'use strict';
+const SW_VERSION ='kuoilgj';
+
+self.addEventListener('activate', event => {
+    event.waitUntil(async function() {
+      // Feature-detect
+      if (self.registration.navigationPreload) {
+        // Enable navigation preloads!
+        await self.registration.navigationPreload.enable();
+      }
+    }());
+  });
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open(`kw-app-${SW_VERSION}`).then(cache => {
+        cache.addAll([
+          '/offline.html'
+        ])
+      })
+    )
+  })
+
+self.addEventListener('fetch', event => {
+  event.respondWith(async function() {
+    // Respond from the cache if we can
+    const cachedResponse = await caches.match(event.request);
+    if (cachedResponse) return cachedResponse;
+
+    // Else, use the preloaded response, if it's there
+    const response = await event.preloadResponse;
+    if (response) return response;
+
+    // Else try the network.
+    return fetch(event.request).then(response => response).catch(
+      error => {
+        if(request.headers.get('Accept').includes('text/html')) {
+          // show offline page
+          return caches.match('/offline.html');
+        }
+      }
+    );
+  }());
+});
