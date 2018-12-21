c5e63e973abf8001a10f5a17a0926ec4ef1d5116
Kraig Walker <kraig_walker@me.com>
2018-12-21 11:03:03 +0000

    refactor fetch

--- src/js/sw.js
+++ src/js/sw.js
@@ -39,35 +39,28 @@ self.addEventListener('install', event => {
   )
 });
 
-self.addEventListener('fetch', function(event) {
+addEventListener('fetch', function(event) {
   event.respondWith(
-    // Try the cache
-    caches.open('kw-app' + SW_VERSION).then(function(cache){
-      cache.match(event.request)
+    caches.match(event.request)
       .then(function(response) {
         if (response) {
-          console.log('return successful response from cache');
-          return response;
+          return response;     // if valid response is found in cache return it
+        } else {
+          return fetch(event.request)     //fetch from internet
+            .then(function(res) {
+              return caches.open('kw-app-' + SW_VERSION)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());    //save the response for future
+                  return res;   // return the fetched data
+                })
+            })
+            .catch(function(err) {       // fallback mechanism
+              return caches.open('kw-app-' + SW_VERSION)
+                .then(function(cache) {
+                  return cache.match('/offline.html');
+                });
+            });
         }
-        return fetch(event.request).then(function(response) {
-          console.log('returning network fetch')
-          console.dir(response);
-          return response;
-        })
-          .catch(function(response) {
-            if (response.status === 404) {
-              console.log('404');
-              return cache.match('/404/index.html');
-            }
-            // If both fail, show a generic fallback:
-            return caches.open('kw-app-' + SW_VERSION)
-              .then(function(cache) { 
-                console.log('handling offline')
-                cache.match('/offline/index.html');
-              })
-        })
       })
-    })
-    )
-  }
-);
+  );
+});      
