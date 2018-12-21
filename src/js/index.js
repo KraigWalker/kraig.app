3ba80874cdde57af37ecba65963fec6981de124d
Kraig Walker <kraig_walker@me.com>
2018-12-21 12:09:43 +0000

    cleanup main index js

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,9 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register(process.env.SW_PATH).then(registration => {
-            console.log('SW registered: ', registration);
-        }).catch(registrationError => {
+        navigator.serviceWorker.register(process.env.SW_PATH)
+        .catch(registrationError => {
             console.log('SW registration failed: ', registrationError);
-        })
+        });
     })
 }
 
